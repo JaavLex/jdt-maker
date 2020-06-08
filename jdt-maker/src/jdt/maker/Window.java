0f1b6e23d913f61896a1054f5c2883c7aea55877
TacticsCH <alexandre.javet@hotmail.com>
2020-06-08 16:11:55 +0200

    [FEAT.] Basic java graphic app template

---
+++ jdt-maker/src/jdt/maker/Window.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jdt.maker;
+
+/**
+ *
+ * @author Alexa
+ */
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import java.awt.* ;
+import javax.swing.* ;
+import javax.swing.event.* ;
+import java.text.DecimalFormat ;
+
+public class Window extends JFrame{
+    
+
+    public Window(){
+        
+    }
+
+}
